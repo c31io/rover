145a5c7abcfc27694b56e8c4293f1eb8f9f8f0a6
Celio Grand <celiogrand@outlook.com>
2022-10-11 19:57:32 +0800

    NavBar w/o responsive layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,29 +16,86 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWidget> createState() => Navigation();
+}
+
+class Navigation extends State<HomeWidget> {
+  static const List<Widget> views = <Widget>[
+    Text(
+      'Index 0',
+    ),
+    Text(
+      'Index 1',
+    ),
+    Text(
+      'Index 2',
+    ),
+    Text(
+      'Index 3',
+    ),
+    Text(
+      'Index 4',
+    ),
+    Text(
+      'Index 5',
+    ),
+  ];
+
+  int viewIndex = 0;
+
+  void onViewTapped(int index) {
+    setState(() {
+      viewIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth > 600) {
-            return buildWideLayout();
-          } else {
-            return buildNarrowLayout();
-          }
-        },
+      body: Center(
+        child: views.elementAt(viewIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Person',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: 'Cloud',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.battery_charging_full),
+            label: 'Pay',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assessment),
+            label: 'Plan',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.api),
+            label: 'Gene',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dataset),
+            label: 'Meme',
+            tooltip: '',
+          ),
+        ],
+        currentIndex: viewIndex,
+        onTap: onViewTapped,
       ),
     );
   }
-}
-
-Widget buildWideLayout() {
-  return const Scaffold();
-}
-
-Widget buildNarrowLayout() {
-  return const Scaffold();
 }
